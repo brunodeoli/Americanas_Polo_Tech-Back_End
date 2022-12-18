d18e1c53c4772ba779518cf12db194b6cca6bd1d
Bruno de Oliveira <75718399+brunodeoli@users.noreply.github.com>
2022-12-18 19:43:25 -0300

    Testando métodos (jogada, verificaPosicao, verificaVelha)

--- src/Modulo1/ProjetoFinal1/JogoDaVelha.java
+++ src/Modulo1/ProjetoFinal1/JogoDaVelha.java
@@ -1,10 +1,29 @@
 package Modulo1.ProjetoFinal1;
 
+import java.util.Scanner;
+
 public class JogoDaVelha {
     public static void main(String[] args) {
+        Scanner sn = new Scanner(System.in);
 
         Jogo jogo = new Jogo();
-        jogo.imprimeTabuleiro();
+        int linha = 0;
+        int coluna = 0;
+
+        jogo.jogada_atual = 8;
+
+        if(jogo.jogada_atual%2==0){
+            System.out.println("Jogador 1, escolha uma casa para marcar com 'O': ");
+            linha = sn.nextInt();
+            coluna = sn.nextInt();
+            jogo.jogada('O', linha, coluna);
+        } else{
+            System.out.println("Jogador 2, escolha uma casa para marcar com 'X': ");
+            linha = sn.nextInt();
+            coluna = sn.nextInt();
+            jogo.jogada('X', linha, coluna);
+        }
+
 
     }
 }
@@ -14,13 +33,21 @@ class Jogo{
     private Character[][] tabuleiro = new Character[3][3];
 
     public void jogada(char jogador, int linha, int coluna){
-        //jogada do jogador 'X'
+        if(verificaPosicao(linha,coluna)){
+            this.tabuleiro[linha][coluna] = jogador;
+            this.jogada_atual++;
+            imprimeTabuleiro();
+            verificaVencedor(jogador);
+            verificaVelha();
+        }
+
+
 
     }
 
     public boolean verificaPosicao(int linha, int coluna){
         //verifica se a posicao que o jogador quer jogar esta livre
-        return false;
+        return (linha >= 0 && linha <= 2) && (coluna >= 0 && coluna <= 2);
     }
 
     public boolean verificaVencedor(char jogador){
@@ -29,7 +56,7 @@ class Jogo{
 
     public boolean verificaVelha(){
         if(this.jogada_atual == 9){
-            System.out.println("O JOGO DEU VELHA! NINGUÉM VENCEU");
+            System.out.println("A PARTIDA DEU VELHA!\nSEM VENCEDOR");
             return true;
         }
         return false;
