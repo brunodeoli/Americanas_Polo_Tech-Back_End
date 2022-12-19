fdf422ff0fbe981ccc46cf8c1fd1c0c76cf36d27
Bruno de Oliveira <75718399+brunodeoli@users.noreply.github.com>
2022-12-18 23:22:31 -0300

    Funcional, verificar melhoras

--- src/Modulo1/ProjetoFinal1/JogoDaVelha.java
+++ src/Modulo1/ProjetoFinal1/JogoDaVelha.java
@@ -53,8 +53,19 @@ class Jogo{
 
     public boolean verificaVencedor(){
         for (int i = 0; i < this.tabuleiro.length; i++) {
-            // verifica linhas
-            if(this.tabuleiro[i][0] != null && this.tabuleiro[i][1] != null && this.tabuleiro[i][2] != null){
+            // verifica linha
+            boolean verifica_linha = this.tabuleiro[i][0] != null && this.tabuleiro[i][1] != null && this.tabuleiro[i][2] != null;
+            if(verifica_linha){
+                char marcacao = this.tabuleiro[i][0];
+                if ((marcacao == this.tabuleiro[i][1]) && (marcacao == this.tabuleiro[i][2])) {
+                    int jogador = marcacao == 'O' ? 1 : 2;
+                    System.out.println("JOGADOR " + jogador + " VENCEU!");
+                    return true;
+                }
+            }
+            // verifica coluna
+            boolean verifica_coluna = this.tabuleiro[0][i] != null && this.tabuleiro[1][i] != null && this.tabuleiro[2][i] != null;
+            if(verifica_coluna){
                 char marcacao = this.tabuleiro[0][i];
                 if ((marcacao == this.tabuleiro[1][i]) && (marcacao == this.tabuleiro[2][i])) {
                     int jogador = marcacao == 'O' ? 1 : 2;
@@ -62,17 +73,28 @@ class Jogo{
                     return true;
                 }
             }
-            // verifica colunas
-            if(this.tabuleiro[0][i] != null && this.tabuleiro[1][i] != null && this.tabuleiro[2][i] != null){
-                char marcacao = this.tabuleiro[0][i];
-                if ((marcacao == this.tabuleiro[1][i]) && (marcacao == this.tabuleiro[2][i])) {
-                    int jogador = marcacao == 'O' ? 1 : 2;
-                    System.out.println("JOGADOR " + jogador + " VENCEU!");
-                    return true;
-                }
-            }
-            //verifica diagonal
         }
+        //verifica diagonal
+        boolean diagonal = this.tabuleiro[0][0] != null && this.tabuleiro[1][1] != null && this.tabuleiro[2][2] != null;
+        if(diagonal){
+            char marcacao = this.tabuleiro[1][1];
+            if (marcacao == this.tabuleiro[0][0] && marcacao == this.tabuleiro[2][2]) {
+                int jogador = marcacao == 'O' ? 1 : 2;
+                System.out.println("JOGADOR " + jogador + " VENCEU!");
+                return true;
+            }
+        }
+        //verifica diagonal invertida
+        boolean diagonal_invertida = this.tabuleiro[0][2] != null && this.tabuleiro[1][1] != null && this.tabuleiro[2][0] != null;
+        if(diagonal_invertida){
+            char marcacao = this.tabuleiro[1][1];
+            if (marcacao == this.tabuleiro[0][2] && marcacao == this.tabuleiro[2][0]) {
+                int jogador = marcacao == 'O' ? 1 : 2;
+                System.out.println("JOGADOR " + jogador + " VENCEU!");
+                return true;
+            }
+        }
+        // retorna falso se não encontrou vencedor
         return false;
     }
 
@@ -85,7 +107,7 @@ class Jogo{
     }
 
     public void imprimeTabuleiro(){
-        System.out.println("   RODADA "+this.rodada);
+        System.out.println("   RODADA "+this.rodada+"\n-------------");
         for (int i = 0; i < this.tabuleiro.length; i++) {
             Character[] linha = this.tabuleiro[i];
             System.out.print("[ ");
@@ -101,6 +123,7 @@ class Jogo{
                 }else{
                     System.out.print(" | ");
                 }
+
             }
         }
         System.out.println("-------------\n");
